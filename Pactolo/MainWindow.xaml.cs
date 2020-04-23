7c21f812f9a7215ea61df97af3839717337b0bb9
Luan <akaluan.la@gmail.com>
2020-04-23 19:38:36 -0300

    Testando o block input e código comentado

--- Pactolo/MainWindow.xaml.cs
+++ Pactolo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.ComponentModel;
 using System.Windows;
 using WpfAppBar;
@@ -11,43 +12,47 @@ namespace Pactolo
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
+        //https://stackoverflow.com/questions/586547/how-can-i-block-keyboard-and-mouse-input-in-c testando o BlockInput
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "BlockInput")]
+        [return: System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        public static extern bool BlockInput([System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)] bool fBlockIt);
+        //criando variáveis que correspondem a biblioteca EventHook
         private readonly EventHookFactory eventHookFactory = new EventHookFactory();
         private readonly MouseWatcher mouseWatcher;
+        //variáveis gerais e flags
         public bool dblClick = false;
-        public bool mwAtivado, noClick = true;
+        public bool mwAtivado, click = true;
+        //variável referente a biblioteca do Quellatalo que faz que simula as ações do mouse
         public MouseHandler mouse = new MouseHandler();
         private const int WM_LBUTTONDOWN = 0x0201;
 
+
+        //Inicializando a janela principal da aplicação
         public MainWindow()
         {
             InitializeComponent();
             System.Windows.Application.Current.Exit += OnApplicationExit;
 
+            //Assim que o aplicativo começa a rodar, o mouseWatcher começa a receber os eventos do mouse
             mouseWatcher = eventHookFactory.GetMouseWatcher();
             mouseWatcher.Start();
         }
 
-        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            switch (msg)
-            {
-                case WM_LBUTTONDOWN:
-                    // MouseMove?.Invoke();
-                    break;
-            }
-
-            return IntPtr.Zero;
-        }
+        
 
         private void Grid_Loaded_1(object sender, RoutedEventArgs e)
         {
+            //Seta a janela pra fiar ancorada no canto esquerdo da tela (ABEdge.Left). Isso diz respeito a biblioteca WpfAppBar
             AppBarFunctions.SetAppBar(this, ABEdge.Left);
         }
 
         private void App_Close(object sender, RoutedEventArgs e)
         {
+            //Função p normalizar a ancoragem quando a aplicação fechar e não dar bug no Explorer
             AppBarFunctions.SetAppBar(this, ABEdge.None);
             this.Close();
         }
@@ -55,6 +60,7 @@ namespace Pactolo
         private void DoubleClick(object sender, RoutedEventArgs e)
         {             
             dblClick = true;
+            //Qnd o botão de duplo clique é iniciado, o mouse começa a ser vigiado. Se a flag estiver certinha, a função manda um click pro sistema assim que detectar uma mensagem ''WM_LBUTTONUP''
             mouseWatcher.OnMouseInput += (s, r) => {
                 //Console.WriteLine(r.Message.ToString());
                 if (dblClick == true)
@@ -72,40 +78,31 @@ namespace Pactolo
                 
             };
         }
+
+        //função p parar o clique em teste
+
         private void StopClick(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            if (noClick == true)
+            if (click == true)
             {
-                if (mwAtivado == false) {
-                    mouseWatcher.Start();
-                } 
-                Console.WriteLine("entrou");
-                noClick = false;
-                mouseWatcher.OnMouseInput += (s, r) =>
-                {
-                    if (noClick == true) {
-                        return;
-                    }
-                    if (r.Message.ToString() == "WM_LBUTTONDOWN")
-                    {
-                        HwndSource.FromHwnd(new WindowInteropHelper(this).Handle)?.AddHook(this.WndProc);
-                        Console.WriteLine("impediu o clique");
-                    }
-                };
-            } else if (noClick == false)
+                Console.WriteLine("false");
+                MainWindow.BlockInput(true);
+                click = false;
+               
+            } else if (click == false)
               {
-                  noClick = true;
-                  Console.WriteLine("saiu");
-                  return;
+                Console.WriteLine("true");
+                click = true;
+                MainWindow.BlockInput(false);
+                return;
               }                                               
         }
 
         private void OnApplicationExit(object sender, EventArgs e)
         {
+            //finaliza o mouseWatcher
             mouseWatcher.Stop();
             eventHookFactory.Dispose();
         }
-
     }
-
 }
